c2161fd52b6cc9fb9c74c3ee44586ef5c416ef42
KamelTouati <kameltouati19.work@gmail.com>
2023-07-04 19:50:12 +0100

    make some modifications

--- django_chatbot/urls.py
+++ django_chatbot/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chatbot.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
